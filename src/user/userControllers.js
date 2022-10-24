d79ec93f6e191b7d2bb3c05a4f8de1d90eabcba8
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-24 16:32:03 +0100

    add readUser and createUser functions

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -1,12 +1,21 @@
 const User = require("./userModel")
 
 exports.createUser = async (req, res) => {
-    console.log(req)
     try {
         const newUser = await User.create(req.body)
         res.status(201).send({user: newUser})
     } catch (error) {
         console.log(error).send({error: error.message})
         res.status(500)
+    }
+}
+
+exports.readUser = async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.status(200).send({user: users})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({error: error.message})
     }
 }
--- src/user/userRouters.js
+++ src/user/userRouters.js
@@ -1,9 +1,9 @@
 const { Router } = require("express")
-const { createUser, readUsers } = require("./userControllers")
+const { createUser, readUser } = require("./userControllers")
 
 const userRouter = Router()
 
-// userRouter.get("/readUser", readUsers)
+userRouter.get("/readUser", readUser)
 userRouter.post("/createUser", createUser)
 
 module.exports = userRouter
