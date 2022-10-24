68c0af54dff68e998ba7388b46d81c85ace3fffd
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-24 15:52:36 +0100

    add content to userRouter & server

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const connection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        console.log("Successfully connected.")
+        console.log("Successfully connected")
     } catch (error) {
         console.log(error)
     }
--- src/user/userRouters.js
+++ src/user/userRouters.js
@@ -3,7 +3,7 @@ const { createUser, readUsers } = require("./userControllers")
 
 const userRouter = Router()
 
-userRouter.get("/readUser", readUsers)
+// userRouter.get("/readUser", readUsers)
 userRouter.post("/createUser", createUser)
 
 module.exports = userRouter
