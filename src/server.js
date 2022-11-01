cc57f3c837c5a5ff18b39b1b3aee67184405c414
jdaniels98 <jackdanielswork98@gmail.com>
2022-11-01 12:06:42 +0000

    add link to react

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 require("./db/connection")
-
+const cors = require("cors")
 const express = require("express")
 const userRouter = require("./user/userRouters")
 
 const app = express()
 const port = process.env.PORT || 5001
 
+app.use(cors())
 app.use(express.json())
-
 app.use(userRouter)
 
 app.listen(port, () => {
