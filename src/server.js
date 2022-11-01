c65fe0abecf445a87b46e18c939b9b225b8db60f
jdaniels98 <jackdanielswork98@gmail.com>
2022-11-01 11:21:04 +0000

    structure change

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ const app = express()
 const port = process.env.PORT || 5001
 
 app.use(express.json())
+
 app.use(userRouter)
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
